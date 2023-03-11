0d99976035bcba8d22afa7af0725b52fd3cd5071
arielcollatti <colatti2017@gmail.com>
2023-03-10 21:44:23 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("");
+Console.WriteLine("");
+
+Console.WriteLine("Noite. Um silvo no ar,");
+Console.WriteLine("Ninguém na estação. E o trem");
+Console.WriteLine("passa sem parar.");
+
+
+
+
+
